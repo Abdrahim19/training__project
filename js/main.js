184892a50fba058d89403657aece431fd0344935
hamzaAbdrahim <rsstrr7@gmail.com>
2023-12-31 16:10:34 +0100

    'push__website'

---
+++ js/main.js
@@ -0,0 +1,6 @@
+fetch('https://dummyjson.com/products')
+.then(res => res.json())
+.then(json => console.log(json))
+let p = document.createElement('p').setAttribute('className' , 'hamza');
+p.innerHtml='hamza'
+
